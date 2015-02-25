0d8f3957661bea6294ec6b7bbf7dc7db3f3bbaf1
A. Diamond <diamond@sheprador.com>
2015-02-25 14:15:11 -0500

    Serializer tests are working

--- dpnode/dpn/api/tests/test_serializers.py
+++ dpnode/dpn/api/tests/test_serializers.py
@@ -7,43 +7,70 @@
 
 import json
 from django.test import TestCase
-from dpn.api.serializers import CreateTransferSerializer
-from dpn.api.serializers import BasicTransferSerializer, NodeSerializer
-from dpn.api.serializers import RegistryEntrySerializer
+from dpn.api.serializers import CreateReplicationSerializer, CreateRestoreSerializer
+from dpn.api.serializers import BasicReplicationSerializer, NodeSerializer
+from dpn.api.serializers import BagSerializer, BasicRestoreSerializer
 
 
-from dpn.data.models import RegistryEntry, Node, Protocol
-from dpn.data.tests.utils import make_registry_data, make_test_nodes
+from dpn.data.models import Bag, Node, Protocol
+from dpn.data.tests.utils import make_bag_data, make_test_nodes
 
 # Data Fixtures
 # The point here is to use the exact same structures posted to the Wiki
 # so we can be sure our documented data structures always pass.
 
 # https://github.com/APTrust/EarthDiver/wiki/API-Endpoint:-Transfer#post-operation-internal
-XFER_POST = json.loads("""{
-    "dpn_object_id":"8044ea1a-cba5-44e7-a4d5-122f649f81a4",
+REPL_POST = json.loads("""{
+    "bag":"8044ea1a-cba5-44e7-a4d5-122f649f81a4",
     "link":"sshaccount@dpnserver.test.org:8044ea1a-cba5-44e7-a4d5-122f649f81a4.tar",
-    "node":"chron",
+    "to_node":"chron",
+    "from_node":"aptrust",
     "size":3239653228,
-    "exp_fixity":"eced7e98269750587531381811b2a59f"
+    "fixity_algorithm": "sha256",
+    "protocol": "R"
 }""")
 
 # https://github.com/APTrust/EarthDiver/wiki/API-Endpoint:-Transfer#get-operation-external
-XFER_DATA = json.loads("""{
-            "node":"chron",
-            "dpn_object_id":"8287ee9e-74f1-490b-8209-800985361134",
-            "status":"P",
-            "event_id":"aptrust-1665",
+REPL_DATA = json.loads("""{
+            "from_node":"aptrust",
+            "to_node": "tdr",
+            "uuid":"8287ee9e-74f1-490b-8209-800985361134",
+            "status":"Requested",
+            "replication_id":"aptrust-1665",
             "protocol":"R",
             "link":"chrondpn@dpn.nodename.org:outgoing/8287ee9e-74f1-490b-8209-800985361134.tar",
-            "size":543891175,
-            "fixity_type": "sha256",
-            "receipt":null,
-            "fixity":null,
-            "valid":null,
+            "fixity_algorithm": "sha256",
+            "fixity_value":null,
+            "fixity_accept":null,
+            "bag_valid":null,
             "created_on":"2014-11-11T00:27:20.295603Z",
             "updated_on":"2014-11-11T00:27:20.297468Z"
         }""")
+
+
+RESTORE_POST = json.loads("""{
+    "bag":"8044ea1a-cba5-44e7-a4d5-122f649f81a4",
+    "to_node":"chron",
+    "from_node":"aptrust",
+    "protocol": "R"
+}""")
+
+RESTORE_DATA = json.loads("""{
+            "from_node":"aptrust",
+            "to_node": "tdr",
+            "bag":"8287ee9e-74f1-490b-8209-800985361134",
+            "status":"Requested",
+            "replication_id":"aptrust-1665",
+            "protocol":"R",
+            "link":"chrondpn@dpn.nodename.org:outgoing/8287ee9e-74f1-490b-8209-800985361134.tar",
+            "fixity_algorithm": "sha256",
+            "fixity_value":null,
+            "fixity_accept":null,
+            "bag_valid":null,
+            "created_on":"2014-11-11T00:27:20.295603Z",
+            "updated_on":"2014-11-11T00:27:20.297468Z"
+        }""")
+
 
 # https://github.com/APTrust/EarthDiver/wiki/API-Endpoint:-Node
 NODE_DATA = json.loads("""{
@@ -60,57 +87,90 @@ NODE_DATA = json.loads("""{
     "namespace":"aptrust",
     "api_root":"https://dpn-dev.aptrust.org/dpnrest/",
     "ssh_pubkey":null,
-    "replicate_from":true,
-    "replicate_to":true,
-    "restore_from":false,
-    "restore_to":false,
+    "replicate_from": ["tdr", "sdr", "chron", "hathi"],
+    "replicate_to": ["tdr", "sdr", "chron", "hathi"],
+    "restore_from": ["tdr", "sdr", "chron", "hathi"],
+    "restore_to": ["tdr", "sdr", "chron", "hathi"],
     "created_on":"2014-11-11T00:27:16.093555Z",
     "updated_on":"2014-11-11T00:27:16.093586Z"
 }""")
 
-REGISTRY_DATA = json.loads("""{
-    "first_node":"sdr",
-    "brightening_objects":[],
-    "rights_objects":[],
+BAG_DATA = json.loads("""{
+    "original_node":"sdr",
+    "admin_node":"sdr",
+    "brightening":[],
+    "rights":[],
     "replicating_nodes":[],
-    "dpn_object_id":"892c3cca-4e18-4873-9c7e-758f5d17a5e9",
+    "uuid":"892c3cca-4e18-4873-9c7e-758f5d17a5e9",
     "local_id":null,
     "version_number":1,
     "creation_date":"2014-11-11T00:27:16.206545Z",
     "last_modified_date":"2014-11-11T00:27:16.206550Z",
     "bag_size":2158502848,
     "object_type":"D",
-    "previous_version":null,
-    "forward_version":null,
-    "first_version":"892c3cca-4e18-4873-9c7e-758f5d17a5e9"
+    "first_version_uuid":"892c3cca-4e18-4873-9c7e-758f5d17a5e9",
+    "size": 78776,
+    "fixities": [
+        {
+          "algorithm": "sha256",
+          "digest": "765765765765",
+          "created_at": "2015-02-25T15:27:40.383861Z"
+        }]
 }""")
 
 #****** TESTS ******#
 
-class CreateTransferSerializerTest(TestCase):
+class CreateReplicationSerializerTest(TestCase):
 
     def test_data(self):
-        xfer = CreateTransferSerializer(data=XFER_POST)
+        repl = CreateReplicationSerializer(data=REPL_POST)
         # It should fail to validate if related objects not created.
-        self.assertFalse(xfer.is_valid(), "Did not expect this to validate!")
+        self.assertFalse(repl.is_valid(), "Did not expect this to validate!")
 
         make_test_nodes()
-        data = make_registry_data()
-        data['dpn_object_id'] = XFER_POST["dpn_object_id"]
-        data['first_node'] = Node.objects.get(namespace=XFER_POST["node"])
-        re = RegistryEntry(**data)
+        data = make_bag_data()
+        data['uuid'] = REPL_POST["bag"]
+        data['original_node'] = Node.objects.get(namespace=REPL_POST["from_node"])
+        data['admin_node'] = Node.objects.get(namespace=REPL_POST["from_node"])
+        re = Bag(**data)
         re.save()
 
         # It should validate with good data.
-        xfer = CreateTransferSerializer(data=XFER_POST)
-        self.assertTrue(xfer.is_valid(), xfer._errors)
+        repl = CreateReplicationSerializer(data=REPL_POST)
+        self.assertTrue(repl.is_valid(), repl._errors)
 
-class BasicTransferSerializerTest(TestCase):
+class BasicReplicationSerializerTest(TestCase):
 
     def test_data(self):
         # It should validate based on this structure.
-        xfer = BasicTransferSerializer(data=XFER_DATA)
-        self.assertTrue(xfer.is_valid())
+        repl = BasicReplicationSerializer(data=REPL_DATA)
+        self.assertTrue(repl.is_valid(), repl._errors)
+
+class CreateRestoreSerializerTest(TestCase):
+
+    def test_data(self):
+        restore = CreateReplicationSerializer(data=RESTORE_POST)
+        # It should fail to validate if related objects not created.
+        self.assertFalse(restore.is_valid(), "Did not expect this to validate!")
+
+        make_test_nodes()
+        data = make_bag_data()
+        data['uuid'] = RESTORE_POST["bag"]
+        data['original_node'] = Node.objects.get(namespace=RESTORE_POST["from_node"])
+        data['admin_node'] = Node.objects.get(namespace=RESTORE_POST["from_node"])
+        re = Bag(**data)
+        re.save()
+
+        # It should validate with good data.
+        restore = CreateReplicationSerializer(data=RESTORE_POST)
+        self.assertTrue(restore.is_valid(), restore._errors)
+
+class BasicRestoreSerializerTest(TestCase):
+
+    def test_data(self):
+        # It should validate based on this structure.
+        restore = BasicRestoreSerializer(data=RESTORE_DATA)
+        self.assertTrue(restore.is_valid(), restore._errors)
 
 class NodeSerializerTest(TestCase):
 
@@ -120,9 +180,9 @@ class NodeSerializerTest(TestCase):
         ptcl.save()
         self.assertTrue(node.is_valid(), node._errors)
 
-class RegistrySerializerTest(TestCase):
+class BagSerializerTest(TestCase):
 
     def test_data(self):
         make_test_nodes()
-        reg = RegistryEntrySerializer(data=REGISTRY_DATA)
+        reg = BagSerializer(data=BAG_DATA)
         self.assertTrue(reg.is_valid(), reg._errors)
